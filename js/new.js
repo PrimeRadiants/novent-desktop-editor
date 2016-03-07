2cb973258c3d83ae20aec267653302261fb2b0a2
Georges Biaux <gbiaux@datawords.com>
2016-03-07 18:19:36 +0100

    setting new project dialog

--- js/new.js
+++ js/new.js
@@ -1,7 +1,27 @@
 var app = angular.module('app', []);
-const ipcRenderer = require('electron').ipcRenderer;
-const shell = require('electron').shell;
+const electron = require('electron');
+const ipcRenderer = electron.ipcRenderer;
+const remote = require('remote');
+const dialog = electron.dialog;
 
 app.controller('newController', function($scope) {
+	$scope.projectLocation = remote.getGlobal('newProjectPath');
 	
+	$scope.$watch(function() { return remote.getGlobal('newProjectPath'); }, function(value) {
+		$scope.projectLocation = remote.getGlobal('newProjectPath');
+	});
+	
+	$scope.chooseProjectLocation = function() {
+		$scope.projectLocation = ipcRenderer.sendSync('choose-project-location', '');
+	}
+	
+	$scope.cancel = function() {
+		ipcRenderer.sendSync('new-project-close', '');
+	}
+	
+	$scope.create = function() {
+		if($scope.projectName != undefined && $scope.projectName != null && $scope.projectName != "")
+			ipcRenderer.sendSync('create-new-project', {name: $scope.projectName, location: $scope.projectLocation});
+		
+	}
 });
--- main.js
+++ main.js
@@ -12,8 +12,10 @@ const ipcMain = electron.ipcMain;
 
 let dialogWindow;
 let mainWindow;
+let newProjectWindow;
 
 global.filePath = null;
+global.newProjectPath = app.getPath("documents");
 
 function main() {
 	if(global.filePath != null && path.extname(process.argv[1]) == ".noventproj")
@@ -39,7 +41,7 @@ function openDialog () {
   });
   
   ipcMain.on('dialog-create-new-project', function(event, arg) {
-	  createNewProject();
+	  openCreateNewProjectDialog();
   });
   
   dialogWindow.on('closed', function() {
@@ -58,7 +60,13 @@ function openEditor() {
 			mainWindow.maximize();
 			mainWindow.loadURL('file://' + __dirname + '/index.html');
 			mainWindow.webContents.openDevTools();
-			dialogWindow.close();
+			
+			if(dialogWindow != null)
+				dialogWindow.close();
+			
+			if(newProjectWindow != null)
+				newProjectWindow.close();
+			
 			mainWindow.on('closed', function() {
 				mainWindow = null;
 			});
@@ -84,38 +92,65 @@ function openExistingProject() {
 	});
 }
 
-function createNewProject() {
-	dialog.showOpenDialog({properties: ['openDirectory']}, function(filePaths) {
+function openCreateNewProjectDialog() {
+	if(dialogWindow != null)
+		dialogWindow.hide();
+	newProjectWindow = new BrowserWindow({width: 716, height: 400, resizable: false});
+	newProjectWindow.setMenu(null);
+	newProjectWindow.loadURL('file://' + __dirname + '/new.html');
+	
+	ipcMain.on('new-project-close', function(event, arg) {
+		newProjectWindow.close();
+		if(dialogWindow != null)
+			dialogWindow.show();
+	});
+	
+	ipcMain.on('choose-project-location', function(event, arg) {
+		dialog.showOpenDialog({properties: ['openDirectory']}, function(filePaths) {
+			//If no file selected, quit
+			if(filePaths == undefined) {
+				return;
+			}
+					
+			event.returnValue = filePaths[0];
+		});
+	});
+	
+	ipcMain.on('create-new-project', function(event, arg) {
+		if(dialogWindow != null)
+			dialogWindow.close();
+		createNewProject(arg.name, arg.location);
+	});
+	
+	newProjectWindow.on('closed', function() {
+		newProjectWindow = null;
+	});
+}
 
-		//If no file selected, quit
-		if(filePaths == undefined) {
-			return;
-		}
-
-		var projectPath = filePaths[0];
-		
-		fs.writeFile(projectPath + "/" + path.basename(projectPath) + '.noventproj', '', (err) => {
+function createNewProject(projectName, projectPath) {
+	fs.mkdir(projectPath + "/" + projectName, (err) => {
+		fs.writeFile(projectPath + "/" + projectName + "/" + projectName + '.noventproj', '', (err) => {
 		  if (err) {
-			openErrorWindow(err);
+			dialog.showErrorBox("Novent project error", JSON.stringify(err));
 			return;  
 		  }
 		  
-		  fs.copy(__dirname + "/project-resources/sample-descriptor.xml", projectPath + "/novent-descriptor.xml", function (err2) {
+		  fs.copy(__dirname + "/project-resources/sample-descriptor.xml", projectPath + "/" + projectName + "/novent-descriptor.xml", function (err2) {
 			  if (err2) {
-				openErrorWindow(err2);
+				dialog.showErrorBox("Novent project error", JSON.stringify(err2));
 				return;  
 			  }
-			  fs.copy(__dirname + "/project-resources/button.png", projectPath + "/images/button.png", function (err3) {
+			  fs.copy(__dirname + "/project-resources/button.png", projectPath + "/" + projectName + "/images/button.png", function (err3) {
 				  if (err3) {
-					openErrorWindow(err3);
+					dialog.showErrorBox("Novent project error", JSON.stringify(err3));
 					return;  
 				  }
-				  fs.copy(__dirname + "/project-resources/begin.png", projectPath + "/images/begin.png", function (err4) {
+				  fs.copy(__dirname + "/project-resources/begin.png", projectPath + "/" + projectName + "/images/begin.png", function (err4) {
 					  if (err4) {
-						openErrorWindow(err4);
+						dialog.showErrorBox("Novent project error", JSON.stringify(err4));
 						return;  
 					  }
-					  global.filePath = projectPath + "/" + path.basename(projectPath) + '.noventproj';
+					  global.filePath = projectPath + "/" + projectName + "/" + projectName + '.noventproj';
 					  openEditor();
 				  });
 			  });
