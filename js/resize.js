8ad1d74abb57445afac548112b648919e184f640
Georges Biaux <gbiaux@datawords.com>
2016-03-09 13:02:15 +0100

    setting section resize

--- js/resize.js
+++ js/resize.js
@@ -1,16 +1,30 @@
+window.$ = require("./js/jquery-2.2.1.min.js");
+
 $(document).ready(function() {
 	var resizeEditorPreview = false;
 	
 	$(".resize-editor-preview").mousedown(function() {
 		resizeEditorPreview = true;
-	}, function() {
-		resizeEditorPreview = false;
 	});
 	
-	$(".resize-editor-preview").mousemove(function(event) {
-		console.log("a");
+	$("#top-section").mouseup(function() {
+		resizeEditorPreview = false;
+		$("#top-section").css("cursor", "default");
+	});
+	
+	/*$(".resize-editor-preview").mouseleave(function() {
+		resizeEditorPreview = false;
+	});*/
+	
+	$("#top-section").mousemove(function(event) {
 		if(resizeEditorPreview) {
-			console.log("test");
+			$("#top-section").css("cursor", "ew-resize");
+			var percent = event.clientX/window.innerWidth;
+			
+			if(percent < 0.9 && percent > 0.1) {
+				$("#editor").css("right", (1 - percent)*100 + "%");
+				$("#preview").css("left", percent*100 + "%");
+			}
 		}
 	});
 });
