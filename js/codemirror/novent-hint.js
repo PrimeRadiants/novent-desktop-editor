0bace55bb3b6d7dbdf248580da0b4c948c6158f6
Georges Biaux <gbiaux@datawords.com>
2016-03-31 11:22:39 +0200

    updating novent-hints

--- js/codemirror/novent-hint.js
+++ js/codemirror/novent-hint.js
@@ -1,252 +1,163 @@
 var noventTags = {
-  "!top": [
-    "novent"
-  ],
-  "novent": {
-    "attrs": {
-      "height": null,
-      "width": null
+    "!top": ["novent"],
+    "novent": {
+        "attrs": {
+            "height": null,
+            "width": null
+        },
+        "children": ["button", "page"]
     },
-    "children": [
-      "button",
-      "page"
-    ]
-  },
-  "button": {
-    "attrs": {
-      "src": null,
-      "alpha": null,
-      "regX": null,
-      "regY": null,
-      "rotation": null,
-      "scaleX": null,
-      "scaleY": null,
-      "skewX": null,
-      "skewY": null,
-      "visible": null,
-      "x": null,
-      "y": null
-    }
-  },
-  "page": {
-    "attrs": {
-      "name": null
+    "button": {
+        "attrs": {
+            "src": null,
+            "alpha": null,
+            "regX": null,
+            "regY": null,
+            "rotation": null,
+            "scaleX": null,
+            "scaleY": null,
+            "skewX": null,
+            "skewY": null,
+            "visible": null,
+            "x": null,
+            "y": null
+        }
     },
-    "children": [
-      "materials",
-      "event"
-    ]
-  },
-  "materials": {
-    "children": [
-      "image",
-      "animation",
-      "video",
-      "sound",
-      "text"
-    ]
-  },
-  "image": {
-    "attrs": {
-      "name": null,
-      "src": null,
-      "alpha": null,
-      "regX": null,
-      "regY": null,
-      "rotation": null,
-      "scaleX": null,
-      "scaleY": null,
-      "skewX": null,
-      "skewY": null,
-      "visible": null,
-      "x": null,
-      "y": null
-    }
-  },
-  "animation": {
-    "attrs": {
-      "name": null,
-      "src": null,
-      "height": null,
-      "width": null,
-      "frames": null,
-      "framerate": null,
-      "alpha": null,
-      "regX": null,
-      "regY": null,
-      "rotation": null,
-      "scaleX": null,
-      "scaleY": null,
-      "skewX": null,
-      "skewY": null,
-      "visible": null,
-      "x": null,
-      "y": null
-    }
-  },
-  "video": {
-    "attrs": {
-      "name": null,
-      "src": null,
-      "alpha": null,
-      "regX": null,
-      "regY": null,
-      "rotation": null,
-      "scaleX": null,
-      "scaleY": null,
-      "skewX": null,
-      "skewY": null,
-      "visible": null,
-      "x": null,
-      "y": null
-    }
-  },
-  "sound": {
-    "attrs": {
-      "name": null,
-      "src": null,
-      "volume": null
-    }
-  },
-  "text": {
-    "attrs": {
-      "name": null,
-      "font": null,
-      "size": null,
-      "width": null,
-      "align": [
-        "left",
-        "center",
-        "right",
-        "justify"
-      ],
-      "lineHeight": null,
-      "color": null,
-      "alpha": null,
-      "regX": null,
-      "regY": null,
-      "rotation": null,
-      "scaleX": null,
-      "scaleY": null,
-      "skewX": null,
-      "skewY": null,
-      "visible": null,
-      "x": null,
-      "y": null
-    }
-  },
-  "event": {
-    "children": [
-      "end",
-      "animate",
-      "play",
-      "wait",
-      "stop"
-    ]
-  },
-  "end": {},
-  "animate": {
-    "attrs": {
-      "name": null,
-      "target": null,
-      "property": [
-        "volume",
-        "alpha",
-        "regX",
-        "regY",
-        "rotation",
-        "scaleX",
-        "scaleY",
-        "skewX",
-        "skewY",
-        "x",
-        "y"
-      ],
-      "value": null,
-      "duration": null,
-      "ease": [
-        "backIn",
-        "backInOut",
-        "backOut",
-        "bounceIn",
-        "bounceInOut",
-        "bounceOut",
-        "circIn",
-        "circInOut",
-        "circOut",
-        "cubicIn",
-        "cubicInOut",
-        "cubicOut",
-        "elasticIn",
-        "elasticInOut",
-        "elasticOut",
-        "linear",
-        "quadIn",
-        "quadInOut",
-        "quadOut",
-        "quartIn",
-        "quartInOut",
-        "quartOut",
-        "quintIn",
-        "quintInOut",
-        "quintOut",
-        "sineIn",
-        "sineInOut",
-        "sineOut"
-      ],
-      "loop": null,
-      "wiggle": null
+    "page": {
+        "attrs": {
+            "name": null
+        },
+        "children": ["materials", "event"]
     },
-    "children": [
-      "end",
-      "animate",
-      "play",
-      "wait",
-      "stop"
-    ]
-  },
-  "play": {
-    "attrs": {
-      "target": null,
-      "loop": [
-        "loop",
-        "stop",
-        "remove"
-      ]
+    "materials": {
+        "children": ["image", "animation", "video", "sound", "text"]
     },
-    "children": [
-      "end",
-      "animate",
-      "play",
-      "wait",
-      "stop"
-    ]
-  },
-  "wait": {
-    "attrs": {
-      "duration": null
+    "image": {
+        "attrs": {
+            "name": null,
+            "src": null,
+            "alpha": null,
+            "regX": null,
+            "regY": null,
+            "rotation": null,
+            "scaleX": null,
+            "scaleY": null,
+            "skewX": null,
+            "skewY": null,
+            "visible": null,
+            "x": null,
+            "y": null
+        }
     },
-    "children": [
-      "end",
-      "animate",
-      "play",
-      "wait",
-      "stop"
-    ]
-  },
-  "stop": {
-    "attrs": {
-      "target": null,
-      "wait": null
+    "animation": {
+        "attrs": {
+            "name": null,
+            "src": null,
+            "height": null,
+            "width": null,
+            "frames": null,
+            "framerate": null,
+            "alpha": null,
+            "regX": null,
+            "regY": null,
+            "rotation": null,
+            "scaleX": null,
+            "scaleY": null,
+            "skewX": null,
+            "skewY": null,
+            "visible": null,
+            "x": null,
+            "y": null
+        }
     },
-    "children": [
-      "end",
-      "animate",
-      "play",
-      "wait",
-      "stop"
-    ]
-  }
+    "video": {
+        "attrs": {
+            "name": null,
+            "src": null,
+            "alpha": null,
+            "regX": null,
+            "regY": null,
+            "rotation": null,
+            "scaleX": null,
+            "scaleY": null,
+            "skewX": null,
+            "skewY": null,
+            "visible": null,
+            "x": null,
+            "y": null
+        }
+    },
+    "sound": {
+        "attrs": {
+            "name": null,
+            "src": null,
+            "volume": null
+        }
+    },
+    "text": {
+        "attrs": {
+            "name": null,
+            "font": null,
+            "size": null,
+            "width": null,
+            "align": ["left", "center", "right", "justify"],
+            "lineHeight": null,
+            "color": null,
+            "alpha": null,
+            "regX": null,
+            "regY": null,
+            "rotation": null,
+            "scaleX": null,
+            "scaleY": null,
+            "skewX": null,
+            "skewY": null,
+            "visible": null,
+            "x": null,
+            "y": null
+        }
+    },
+    "event": {
+        "children": ["end", "animate", "wiggle", "play", "wait", "stop"]
+    },
+    "end": {},
+    "animate": {
+        "attrs": {
+            "target": null,
+            "property": ["volume", "alpha", "regX", "regY", "rotation", "scaleX", "scaleY", "skewX", "skewY", "x", "y"],
+            "value": null,
+            "duration": null,
+            "ease": ["backIn", "backInOut", "backOut", "bounceIn", "bounceInOut", "bounceOut", "circIn", "circInOut", "circOut", "cubicIn", "cubicInOut", "cubicOut", "elasticIn", "elasticInOut", "elasticOut", "linear", "quadIn", "quadInOut", "quadOut", "quartIn", "quartInOut", "quartOut", "quintIn", "quintInOut", "quintOut", "sineIn", "sineInOut", "sineOut"]
+        },
+        "children": ["end", "animate", "wiggle", "play", "wait", "stop"]
+    },
+    "wiggle": {
+        "attrs": {
+            "name": null,
+            "target": null,
+            "property": ["volume", "alpha", "regX", "regY", "rotation", "scaleX", "scaleY", "skewX", "skewY", "x", "y"],
+            "amplitude": null,
+            "frequency": null,
+            "ease": ["backIn", "backInOut", "backOut", "bounceIn", "bounceInOut", "bounceOut", "circIn", "circInOut", "circOut", "cubicIn", "cubicInOut", "cubicOut", "elasticIn", "elasticInOut", "elasticOut", "linear", "quadIn", "quadInOut", "quadOut", "quartIn", "quartInOut", "quartOut", "quintIn", "quintInOut", "quintOut", "sineIn", "sineInOut", "sineOut"]
+        }
+    },
+    "play": {
+        "attrs": {
+            "target": null,
+            "loop": ["loop", "stop", "remove"]
+        },
+        "children": ["end", "animate", "wiggle", "play", "wait", "stop"]
+    },
+    "wait": {
+        "attrs": {
+            "duration": null
+        },
+        "children": ["end", "animate", "wiggle", "play", "wait", "stop"]
+    },
+    "stop": {
+        "attrs": {
+            "target": null
+        }
+    }
 };
 
 function completeAfter(cm, pred) {
